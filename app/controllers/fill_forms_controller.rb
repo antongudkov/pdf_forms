b21f560b4e090d73feb44c50f0137af5b4586300
Anton Gudkov <xergin@gmail.com>
2021-03-01 17:33:45 +0300

    Fix

--- app/controllers/fill_forms_controller.rb
+++ app/controllers/fill_forms_controller.rb
@@ -1,6 +1,6 @@
 class FillFormsController < ActionController::API
   def fill
-    hypdf_request_params = { user: ENV['HYPDF_USER'], password: ENV['HYPDF_PASSWORD'] }.merge(params.to_hash)
+    hypdf_request_params = { user: ENV['HYPDF_USER'], password: ENV['HYPDF_PASSWORD'] }.merge(fill_params)
 
     hypdf_response = HyPDF.fillform(
       'test/fixtures/files/dental_admission.pdf',
@@ -8,5 +8,11 @@ class FillFormsController < ActionController::API
     )
 
     send_data hypdf_response[:pdf], filename: 'dental_admission_filled.pdf', type: 'application/pdf'
+  end
+
+  private
+
+  def fill_params
+    params.permit(*%i[patient_name patient_sex patient_species patient_breed patient_age patient_birthday patient_weight patient_rabies_tag patient_microchip patient_alerts family_name human_name family_email family_phone family_address family_partner])
   end
 end
